e5f6811182bdfd3e5c3590181c926b887a224c29
Marco <mar.wenzel@gmx.de>
2022-05-22 11:52:47 +0200

    added demo controls

---
+++ lib/WavePainter.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+// Custom Painter Class
+class WavePinter extends CustomPainter {
+  final double time;
+  final double waves;
+  final double segments;
+  final double amplitude;
+  final double waveHorOffset;
+  final Color primColor;
+  final Color secColor;
+
+  WavePinter(
+      {required this.time,
+      required this.waves,
+      required this.segments,
+      required this.amplitude,
+      required this.waveHorOffset,
+      required this.primColor,
+      required this.secColor});
+  @override
+  void paint(Canvas canvas, Size size) {
+    var path = Path();
+    // Additional Parameters
+    const amplitudeVariation = 0.12;
+    const waveVertOffset = 0.0;
+    const waveBaseThickness = 4.0;
+
+    final segmentWidth = size.width / segments;
+    final baseLine = size.height * 0.5;
+    final xShift = size.width * time;
+    final xShift2 = size.width * 1 - time;
+
+    // Draw single wave
+    void drawWave(double amplitude, double horShift, double baseShift, double timeShift) {
+      var wavePath = Path();
+      final ampltudeScale = sin(time * 2 * pi + timeShift * pi);
+      final waveBaseline = baseLine + baseShift;
+
+      void addBezierLine(double start, double widthFraction, double offset, int direction) {
+        wavePath.quadraticBezierTo(start + segmentWidth * (widthFraction + offset), waveBaseline + amplitude * ampltudeScale * direction,
+            start + segmentWidth * (2 * widthFraction + offset), waveBaseline);
+      }
+
+      // Add Bezier points for segments
+      for (var x = 1; x <= segments; x++) {
+        final startPoint = (x - 1) * segmentWidth;
+        wavePath.moveTo(startPoint, waveBaseline);
+        addBezierLine(startPoint, 0.25, 0, 1);
+        addBezierLine(startPoint, 0.25, 0.5, -1);
+      }
+
+      // x shift path over time
+      path.addPath(wavePath, Offset(horShift, 0));
+      path.addPath(wavePath, Offset(-(size.width - horShift), 0));
+    }
+
+    // Draw all waves
+    for (var w = 0; w < waves; w++) {
+      final variation = 1 - (w * amplitudeVariation);
+      drawWave(amplitude * variation, w * waveHorOffset, w * waveVertOffset, variation);
+      path = path.shift(Offset(xShift, 0));
+      path.addPath(path, Offset(-xShift2, 0));
+
+      // Create the paint and draw
+      final paint = createPaint(primColor, secColor, waveBaseThickness, size, variation);
+      canvas.drawPath(path, paint);
+
+      // reset path
+      path = Path();
+    }
+  }
+
+  Paint createPaint(Color primaryColor, Color secondaryColor, double baseStroke, Size size, double variation) {
+    final clear = Colors.white.withOpacity(0);
+    final strokeVariation = sin((time + variation) * 2 * pi) * baseStroke * 0.3;
+    var paint = Paint()
+      ..strokeWidth = baseStroke + strokeVariation
+      ..style = PaintingStyle.stroke
+      ..isAntiAlias = true
+      ..shader = LinearGradient(
+              colors: [clear, secondaryColor, primaryColor, secondaryColor, clear],
+              stops: const [0.2, 0.3, 0.5, 0.7, 0.8],
+              transform: GradientRotation(time * pi + 2 * (variation + 1) * pi))
+          .createShader(Rect.fromCircle(center: Offset(size.width / 2, size.height / 2), radius: size.width / 2));
+    return paint;
+  }
+
+  @override
+  bool shouldRepaint(covariant WavePinter oldDelegate) {
+    return oldDelegate.time != time;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'WavePainter.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,49 +9,182 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Custom Painter',
       theme: ThemeData(
-        primarySwatch: Colors.orange,
+        primarySwatch: Colors.blue,
       ),
-      home: MyPainter(),
+      home: const WaveDemo(),
     );
   }
 }
 
-class MyPainter extends StatelessWidget {
+class WaveDemo extends StatefulWidget {
+  const WaveDemo({Key? key}) : super(key: key);
+
+  @override
+  State<WaveDemo> createState() => _WaveDemoState();
+}
+
+class _WaveDemoState extends State<WaveDemo> with SingleTickerProviderStateMixin {
+  // init values
+  double time = 0.0;
+  double waves = 4;
+  double segments = 1;
+  double amplitude = 50;
+  double waveHorOffset = 15;
+  Color primColor = Colors.red;
+  Color secColor = Colors.orange;
+  final period = 1.0;
+  final duration = 4000;
+  late Animation<double> _animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: Duration(milliseconds: duration), vsync: this);
+    controller.forward();
+    controller.addStatusListener((status) {
+      setAnimationForStatus(status);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    _animation = Tween(begin: 0.0, end: period).animate(controller)
+      ..addListener(() {
+        setState(() {
+          time = _animation.value;
+        });
+      });
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Circles'),
-      ),
-      body: CustomPaint(
-        painter: ShapePainter(),
-        child: Container(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          CustomPaint(
+            size: const Size(double.infinity, 350),
+            painter: WavePinter(
+                time: time,
+                waves: waves,
+                segments: segments,
+                amplitude: amplitude,
+                waveHorOffset: waveHorOffset,
+                primColor: primColor,
+                secColor: secColor),
+          ),
+
+          // Controls for the parameters
+          Column(
+            children: [
+              const Text('Waves', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Slider(
+                  value: waves,
+                  min: 1,
+                  max: 5,
+                  divisions: 4,
+                  label: waves.toInt().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      waves = value;
+                    });
+                  }),
+              const Text('Segments', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Slider(
+                  value: segments,
+                  min: 1,
+                  max: 4,
+                  divisions: 3,
+                  label: segments.toInt().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      segments = value;
+                    });
+                  }),
+              const Text('Amplitude', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Slider(
+                  value: amplitude,
+                  min: 10,
+                  max: 80,
+                  //divisions: 7,
+                  //label: amplitude.toInt().toString(),
+                  onChanged: (double value) {
+                    setState(() {
+                      amplitude = value;
+                    });
+                  }),
+              const Text('Wave X Offset', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              Slider(
+                  value: waveHorOffset,
+                  min: 0,
+                  max: 50,
+                  onChanged: (double value) {
+                    setState(() {
+                      waveHorOffset = value;
+                    });
+                  }),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.red),
+                      child: const Text('Red'),
+                      onPressed: () {
+                        setState(() {
+                          primColor = Colors.red;
+                          secColor = Colors.orange;
+                        });
+                      }),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.yellow),
+                      child: const Text('Yellow'),
+                      onPressed: () {
+                        setState(() {
+                          primColor = Colors.yellow;
+                          secColor = Colors.yellowAccent.shade100;
+                        });
+                      }),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.blue),
+                      child: const Text('Blue'),
+                      onPressed: () {
+                        setState(() {
+                          primColor = Colors.blue;
+                          secColor = Colors.lightBlueAccent;
+                        });
+                      }),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(primary: Colors.greenAccent),
+                      child: const Text('Green'),
+                      onPressed: () {
+                        setState(() {
+                          primColor = Colors.greenAccent.shade400;
+                          secColor = Colors.greenAccent;
+                        });
+                      }),
+                ],
+              )
+            ],
+          )
+        ],
       ),
     );
   }
-}
 
-// FOR PAINTING CIRCLES
-class ShapePainter extends CustomPainter {
   @override
-  void paint(Canvas canvas, Size size) {
-    var paint = Paint()
-      ..color = Colors.orange
-      ..strokeWidth = 5
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round;
-
-    var path = Path();
-
-    path.addOval(Rect.fromCircle(
-      center: Offset(size.width / 2, size.height / 2),
-      radius: 100,
-    ));
-    canvas.drawPath(path, paint);
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
-    return false;
+  void setAnimationForStatus(AnimationStatus status) {
+    switch (status) {
+      case AnimationStatus.completed:
+        controller.reset();
+        break;
+      case AnimationStatus.dismissed:
+        controller.forward();
+        break;
+      default:
+        break;
+    }
   }
 }
